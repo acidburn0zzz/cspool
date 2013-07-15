3a7971ab80af9646e9eb083ffe351cb0e52df28a
ale <ale@incal.net>
2013-07-15 21:28:54 +0000

    add setup.py (distutils)

---
+++ setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+
+setup(name='cspool',
+      version='0.1',
+      author_email='ale@incal.net',
+      packages=['cspool', 'cspool.client', 'cspool.server'],
+      )
