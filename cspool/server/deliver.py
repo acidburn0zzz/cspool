adaef4cbdfec12303d70a93987c2268490b280f6
ale <ale@incal.net>
2013-07-16 00:06:34 +0000

    split code into functions for testability

--- cspool/server/deliver.py
+++ cspool/server/deliver.py
@@ -8,10 +8,13 @@ import sys
 import time
 
 
+def encrypt_msg(msg, user, user_db):
+    return user_db.get_cryptobox(user).encrypt(
+            PutCommand((msg, time.time())).serialize())
+
+
 def deliver(msg, user, user_db):
-    Spool(user).append(
-        user_db.get_cryptobox(user).encrypt(
-            PutCommand((msg, time.time())).serialize()))
+    Spool(user).append(encrypt_msg(msg, user, user_db))
 
 
 def main():
@@ -19,18 +22,23 @@ def main():
     parser.add_option('--user', help='Destination user')
     parser.add_option('--user-key', dest='user_key',
                       help='User public key to use for encryption')
-    parser.add_option('--server-key', dest='server_key',
-                      help='Server secret key to user for signing')
+    parser.add_option('--spool-key', dest='spool_key',
+                      help='Spool secret key to use for signing')
     opts, args = parser.parse_args()
     if not opts.user:
         parser.error('Must specify --user')
     if not opts.user_key:
         parser.error('Must specify --user-key')
-    if not opts.server_key:
-        parser.error('Must specify --server-key')
+    if not opts.spool_key:
+        parser.error('Must specify --spool-key')
 
     msg = sys.stdin.read()
-    user_db = TestUserDb(opts.server_key, opts.user_key)
+    with open(opts.user_key) as fd:
+        public_key = fd.read()
+    with open(opts.spool_key) as fd:
+        private_key = fd.read()
+
+    user_db = TestUserDb(private_key, public_key)
     deliver(msg, opts.user, user_db)
 
 
